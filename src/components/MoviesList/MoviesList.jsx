64ffd8e6bb61afa0295e5d4aaedb1088c00652ab
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-05-20 17:33:57 +0300

    asd

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,5 +1,5 @@
 import css from './MoviesList.module.css'
-
+import { Link } from 'react-router-dom';
 
 export default function HomeCollection({ films }) {
   const imgBaseUrl = 'https://image.tmdb.org/t/p/w500/';
@@ -7,14 +7,14 @@ export default function HomeCollection({ films }) {
   return (
     <ul className={css.movieList}>
       {films.map(({ id, poster_path, name, title }) => (
-        <li className={css.movieItem} key={id}>
+        <Link className={css.movieItem} key={id} to={`/movies/${id}`}>
           <img
             className={css.movieImg}
             src={imgBaseUrl.concat(poster_path)}
             alt={name}
           />
           <span className={css.text}> {title ?? name}</span>
-        </li>
+        </Link>
       ))}
     </ul>
   );
